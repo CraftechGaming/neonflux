2f83bc6931989bd5eb4109ec55145fa31904404c
Hridhan <hridhan.h.gaikwad@gmail.com>
2026-06-11 19:15:39 +0000

    Neonflux is officaly live

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ' ./App.css';
+function app() {
+return (
+<div className="App-header'>
+<h1> NEONFLUX </h1>
+<p>System Online. Welcome to the main control panel.</p>
+<p>Bult directly from an iPad.</p>
+</header
+</div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
