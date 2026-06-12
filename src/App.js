5b4f3c9ab26d44ecbb329355fbefe823bf5a7d91
Hridhan <hridhan.h.gaikwad@gmail.com>
2026-06-12 08:43:18 +0000

    fixed typos and tags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ' ./App.css';
-function app() {
+function App() {
 return (
-<div className="App-header'>
+    <div className="App-header">
 <h1> NEONFLUX </h1>
 <p>System Online. Welcome to the main control panel.</p>
-<p>Bult directly from an iPad.</p>
-</header
+<p>Built directly from an iPad.</p>
 </div>
 );
-
-
+}
