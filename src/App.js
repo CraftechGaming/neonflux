067585bd0dd82cff7e802a5581cc8de9c1a19313
Hridhan <hridhan.h.gaikwad@gmail.com>
2026-06-12 10:25:02 +0000

    Deploy fixed movie streaming app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ' ./App.css';
-function App() {
-return (
-    <div className="App-header">
-<h1> NEONFLUX </h1>
-<p>System Online. Welcome to the main control panel.</p>
-<p>Built directly from an iPad.</p>
-</div>
-);
-}
+404: Not Found
